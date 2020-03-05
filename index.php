7334494e179780e312ec2dd1a75e9eaaea4983fd
gotylergo <tk03759@gmail.com>
2020-03-04 22:09:22 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Hello PHP World</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+  <div class="container">
+    <?php
+    $x = "Hello world!";
+    echo ($x);
+    ?>
+  </div>
+</body>
+
+</html>
